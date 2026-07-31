3c3ae9667ca82f494ea952b6e184e40dd9b61761
LeeSeokmin <dltjrals13@naver.com>
2026-07-31 13:32:52 +0900

    feat: oop 강좌 crud 개발

---
+++ aics-admin/src/main/java/kgu/developers/admin/course/presentation/request/CourseRequest.java
@@ -0,0 +1,27 @@
+package kgu.developers.admin.course.presentation.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+@Builder
+public record CourseRequest(
+	@Schema(description = "과목명", example = "객체지향프로그래밍", requiredMode = REQUIRED)
+	@NotNull
+	String name,
+
+	@Schema(description = "학년도", example = "2026", requiredMode = REQUIRED)
+	@NotNull
+	String year,
+
+	@Schema(description = "학기", example = "FALL", requiredMode = REQUIRED)
+	@NotNull
+	String semester,
+
+	@Schema(description = "상태", example = "DRAFT", requiredMode = REQUIRED)
+	@NotNull
+	String status
+) {
+}
