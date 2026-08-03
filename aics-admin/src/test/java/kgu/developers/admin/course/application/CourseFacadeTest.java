0478fa6d4a2772bfbbed67675abc64ec60833f78
LeeSeokmin <dltjrals13@naver.com>
2026-08-03 12:29:35 +0900

    chore: 깃허브 coverage 액션 통과를 위한 테스트 코드 추가

---
+++ aics-admin/src/test/java/kgu/developers/admin/course/application/CourseFacadeTest.java
@@ -0,0 +1,89 @@
+package kgu.developers.admin.course.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kgu.developers.admin.course.presentation.request.CourseRequest;
+import kgu.developers.domain.course.application.command.CourseCommandService;
+import kgu.developers.domain.course.application.query.CourseQueryService;
+import kgu.developers.domain.course.domain.Course;
+import kgu.developers.domain.course.domain.SemesterType;
+import kgu.developers.domain.course.domain.StatusType;
+
+@ExtendWith(MockitoExtension.class)
+class CourseFacadeTest {
+
+	@Mock
+	private CourseCommandService courseCommandService;
+
+	@Mock
+	private CourseQueryService courseQueryService;
+
+	@InjectMocks
+	private CourseFacade courseFacade;
+
+	private final CourseRequest request = new CourseRequest("객체지향프로그래밍", "2026", "FALL", "DRAFT");
+
+	private Course course() {
+		return Course.create("객체지향프로그래밍", 2026, SemesterType.FALL, StatusType.DRAFT);
+	}
+
+	@Test
+	@DisplayName("createCourse는 문자열 요청을 변환해 커맨드 서비스에 넘기고 id를 응답한다")
+	void createCourse() {
+		given(courseCommandService.createCourse("객체지향프로그래밍", 2026, SemesterType.FALL, StatusType.DRAFT))
+			.willReturn(1L);
+
+		assertThat(courseFacade.createCourse(request).id()).isEqualTo(1L);
+	}
+
+	@Test
+	@DisplayName("updateCourse는 조회한 강좌를 커맨드 서비스에 넘긴다")
+	void updateCourse() {
+		Course course = course();
+		given(courseQueryService.getCourseById(1L)).willReturn(course);
+
+		courseFacade.updateCourse(1L, request);
+
+		verify(courseCommandService).updateCourse(course, "객체지향프로그래밍", 2026, SemesterType.FALL, StatusType.DRAFT);
+	}
+
+	@Test
+	@DisplayName("deleteCourse는 조회한 강좌를 커맨드 서비스에 넘긴다")
+	void deleteCourse() {
+		Course course = course();
+		given(courseQueryService.getCourseById(1L)).willReturn(course);
+
+		courseFacade.deleteCourse(1L);
+
+		verify(courseCommandService).deleteCourse(course);
+	}
+
+	@Test
+	@DisplayName("getCourseById는 쿼리 서비스 결과를 그대로 반환한다")
+	void getCourseById() {
+		Course course = course();
+		given(courseQueryService.getCourseById(1L)).willReturn(course);
+
+		assertThat(courseFacade.getCourseById(1L)).isEqualTo(course);
+	}
+
+	@Test
+	@DisplayName("getAllCourses는 목록을 응답으로 감싸 반환한다")
+	void getAllCourses() {
+		List<Course> courses = List.of(course());
+		given(courseQueryService.getAllCourses()).willReturn(courses);
+
+		assertThat(courseFacade.getAllCourses().contents()).isEqualTo(courses);
+	}
+}
