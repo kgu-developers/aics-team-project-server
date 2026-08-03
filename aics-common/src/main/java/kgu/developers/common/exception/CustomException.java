9f32c545e1fe10ba1cb6b2fdc4b3098ad643b195
LeeSeokmin <dltjrals13@naver.com>
2026-08-03 11:54:29 +0900

    feat: 공통 모듈 기반 클래스 및 예외 처리 추가

---
+++ aics-common/src/main/java/kgu/developers/common/exception/CustomException.java
@@ -0,0 +1,24 @@
+package kgu.developers.common.exception;
+
+import lombok.Getter;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@Getter
+public class CustomException extends RuntimeException {
+	private final ExceptionCode code;
+
+	public CustomException(ExceptionCode code) {
+		super(code.getMessage());
+		this.code = code;
+	}
+
+	public CustomException(ExceptionCode code, Throwable cause) {
+		super(code.getMessage(), cause);
+		this.code = code;
+	}
+
+	public boolean isServerError() {
+		return code.getStatus().equals(INTERNAL_SERVER_ERROR);
+	}
+}
