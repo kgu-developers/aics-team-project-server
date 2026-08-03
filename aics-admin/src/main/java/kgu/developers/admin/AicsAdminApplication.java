d097310514677df5cc4591fb9d123032f74f95d0
LeeSeokmin <dltjrals13@naver.com>
2026-08-03 12:03:44 +0900

    feat: admin application 추가

---
+++ aics-admin/src/main/java/kgu/developers/admin/AicsAdminApplication.java
@@ -0,0 +1,13 @@
+package kgu.developers.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicsAdminApplication.class, args);
+	}
+
+}
