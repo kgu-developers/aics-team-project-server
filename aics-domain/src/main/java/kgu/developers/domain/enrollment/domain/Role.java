c85c706c3967e9b58b3d50013d6fdf5238dc09c1
이석민 <dltjrals13@naver.com>
2026-08-14 15:45:25 +0900

    feature/KD3-84: A 수강생 명단(Enrollment) 엔티티 구현 (#31)
    * feat: enrollment 도메인 및 JPA 영속성 구현
    
    * refactor: enum 파일들의 이름 변경
    
    * refactor: ERD 설계상 studentNumber을 userId로 변경
    
    * refactor: 변경된 userId에 맞춰 함수명 변경

---
+++ aics-domain/src/main/java/kgu/developers/domain/enrollment/domain/Role.java
@@ -0,0 +1,13 @@
+package kgu.developers.domain.enrollment.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    STUDENT("학생"),
+    ASSISTANT("조교");
+
+    private final String description;
+}
---
+++ aics-domain/src/main/java/kgu/developers/domain/enrollment/infrastructure/EnrollmentJpaEntity.java
@@ -0,0 +1,67 @@
+package kgu.developers.domain.enrollment.infrastructure;
+
+import jakarta.persistence.*;
+import kgu.developers.common.domain.BaseTimeEntity;
+import kgu.developers.domain.enrollment.domain.Enrollment;
+import kgu.developers.domain.enrollment.domain.Role;
+import kgu.developers.domain.enrollment.domain.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Table(name = "\"enrollment\"")
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+public class EnrollmentJpaEntity extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long sectionId;
+
+    @Column(nullable = false, length = 20)
+    private String userId;
+
+    @Column(nullable = false, length = 16)
+    @Enumerated(STRING)
+    private Role role;
+
+    @Column(nullable = false, length = 16)
+    @Enumerated(STRING)
+    private Status status;
+
+    public Enrollment toDomain() {
+        return Enrollment.builder()
+                .id(id)
+                .sectionId(sectionId)
+                .userId(userId)
+                .role(role)
+                .status(status)
+                .createdAt(getCreatedAt())
+                .updatedAt(getUpdatedAt())
+                .deletedAt(getDeletedAt())
+                .build();
+    }
+
+    public static EnrollmentJpaEntity toEntity(Enrollment enrollment) {
+        EnrollmentJpaEntity entity = EnrollmentJpaEntity.builder()
+                .id(enrollment.getId())
+                .sectionId(enrollment.getSectionId())
+                .userId(enrollment.getUserId())
+                .role(enrollment.getRole())
+                .status(enrollment.getStatus())
+                .build();
+        entity.createdAt = enrollment.getCreatedAt();
+        entity.setDeletedAt(enrollment.getDeletedAt());
+        return entity;
+    }
+}
