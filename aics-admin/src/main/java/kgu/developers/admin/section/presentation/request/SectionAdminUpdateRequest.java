b7d1bc4eba3dc7ba260c189862b91ebd644796d5
LeeSeokmin <dltjrals13@naver.com>
2026-08-18 13:22:33 +0900

    fix: update request에 @NotBlank 추가

--- aics-admin/src/main/java/kgu/developers/admin/section/presentation/request/SectionAdminUpdateRequest.java
+++ aics-admin/src/main/java/kgu/developers/admin/section/presentation/request/SectionAdminUpdateRequest.java
@@ -3,10 +3,12 @@ package kgu.developers.admin.section.presentation.request;
 import java.time.LocalDateTime;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
 public record SectionAdminUpdateRequest(
     @Schema(description = "교수 학번", example = "202699999")
+    @NotBlank
     String professorId,
 
     @Schema(description = "강좌 ID", example = "1")
@@ -14,12 +16,15 @@ public record SectionAdminUpdateRequest(
     Long courseId,
 
     @Schema(description = "과목 코드", example = "CS101")
+    @NotBlank
     String code,
 
     @Schema(description = "분반명", example = "01")
+    @NotBlank
     String name,
 
     @Schema(description = "수업시간", example = "월123")
+    @NotBlank
     String classTime,
 
     @Schema(description = "정원", example = "40")
