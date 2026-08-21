96490f100275002c9b43a337a7800c38d4a6bbd2
LeeSeokmin <dltjrals13@naver.com>
2026-08-21 20:52:01 +0900

    fix: 사전조사 제출 요청의 preferredRoles 요소를 @NotBlank로 검증

--- aics-api/src/main/java/kgu/developers/api/preSurveyResponse/presentation/request/PreSurveyResponseSubmitRequest.java
+++ aics-api/src/main/java/kgu/developers/api/preSurveyResponse/presentation/request/PreSurveyResponseSubmitRequest.java
@@ -3,6 +3,7 @@ package kgu.developers.api.preSurveyResponse.presentation.request;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.Builder;
@@ -12,7 +13,7 @@ public record PreSurveyResponseSubmitRequest(
 
     @Schema(description = "희망 역할 목록(선호 순서대로)", example = "[\"BACKEND\", \"PM\"]", requiredMode = REQUIRED)
     @NotEmpty
-    List<String> preferredRoles,
+    List<@NotBlank String> preferredRoles,
 
     @Schema(description = "주제 의견", example = "학사 일정 알림 서비스를 만들고 싶습니다")
     String topicOpinion,
