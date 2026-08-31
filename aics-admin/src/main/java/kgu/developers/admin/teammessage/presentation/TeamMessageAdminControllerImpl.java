8f4e99a2adc884fa7896228100bb352fbcd2ef37
sunzx0428@gmail.com <57030648+sunzx0428@users.noreply.github.com>
2026-09-01 04:36:43 +0900

    fix: 통합 쪽지함 컨트롤러 검증 선언을 정리한다

--- aics-admin/src/main/java/kgu/developers/admin/teammessage/presentation/TeamMessageAdminControllerImpl.java
+++ aics-admin/src/main/java/kgu/developers/admin/teammessage/presentation/TeamMessageAdminControllerImpl.java
@@ -1,6 +1,5 @@
 package kgu.developers.admin.teammessage.presentation;
 
-import jakarta.validation.constraints.Positive;
 import kgu.developers.admin.teammessage.application.TeamMessageAdminFacade;
 import kgu.developers.admin.teammessage.presentation.response.TeamMessageAdminPageResponse;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +25,7 @@ public class TeamMessageAdminControllerImpl implements TeamMessageAdminControlle
     @Override
     @GetMapping
     public ResponseEntity<TeamMessageAdminPageResponse> getMessages(
-        @RequestParam(required = false) @Positive Long sectionId,
+        @RequestParam(required = false) Long sectionId,
         @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
         Authentication authentication
     ) {
