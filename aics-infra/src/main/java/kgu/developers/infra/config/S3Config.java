19f201a3421be21bcd233a7f5fda4284aaad17d7
sunzx0428@gmail.com <57030648+sunzx0428@users.noreply.github.com>
2026-09-04 10:17:45 +0900

    fix: S3 빈을 API 서버에서만 활성화한다

--- aics-infra/src/main/java/kgu/developers/infra/config/S3Config.java
+++ aics-infra/src/main/java/kgu/developers/infra/config/S3Config.java
@@ -1,6 +1,7 @@
 package kgu.developers.infra.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -14,6 +15,7 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import kgu.developers.infra.AicsConfig;
 
 @Configuration
+@ConditionalOnProperty(prefix = "aws.s3", name = "enabled", havingValue = "true")
 public class S3Config implements AicsConfig {
 	@Value("${aws.s3.region}")
 	String region;
