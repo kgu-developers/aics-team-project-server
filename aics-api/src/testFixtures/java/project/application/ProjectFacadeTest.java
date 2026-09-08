fad1d703143a5451c571cfe225774832258317c5
LeeSeokmin <dltjrals13@naver.com>
2026-09-08 21:30:03 +0900

    fix: 화면 이미지 일괄 조회 변경에 맞춰 ProjectFacadeTest 스텁 수정

--- aics-api/src/testFixtures/java/project/application/ProjectFacadeTest.java
+++ aics-api/src/testFixtures/java/project/application/ProjectFacadeTest.java
@@ -168,7 +168,8 @@ class ProjectFacadeTest {
     private void givenImageUploadedBy(String uploaderId) {
         given(teamMemberRepository.findAllByTeamId(TEAM_ID))
             .willReturn(List.of(TeamMember.create(TEAM_ID, MEMBER_ID, true, "팀장")));
-        given(fileObjectRepository.findById(1L)).willReturn(Optional.of(FileObject.builder()
+        // 화면 이미지는 N+1을 피하려고 한 번에 조회한다(ProjectFacade.resolveScreenImageUrls).
+        given(fileObjectRepository.findAllById(List.of(1L))).willReturn(List.of(FileObject.builder()
             .id(1L).uploadedBy(uploaderId).storageKey("teams/1/home.png").build()));
     }
 
