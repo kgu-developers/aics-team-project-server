f35f8744875ef76de69d9a05dabd96b36b9bc2aa
LeeSeokmin <dltjrals13@naver.com>
2026-09-17 10:37:37 +0900

    chore: 레거시 컬럼 존재 시에만 비밀번호 변경 필요 상태 승계하도록 수정

--- database/user.sql
+++ database/user.sql
@@ -6,7 +6,15 @@ COMMENT ON COLUMN "user".password_change_required
     IS '관리자 비밀번호 초기화 후 비밀번호 변경 필요 여부';
 
 -- 기존 password_change_required_until 기반 대상자의 상태를 새 컬럼으로 승계한다.
--- password_change_required_until 컬럼이 없는 DB(신규 환경)에서는 이 UPDATE만 실패하며, 승계할 대상도 없으므로 무시해도 된다.
-UPDATE "user"
-SET password_change_required = TRUE
-WHERE password_change_required_until IS NOT NULL;
+-- 레거시 컬럼이 없는 신규 환경에서는 승계 대상이 없으므로 건너뛴다.
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1
+               FROM information_schema.columns
+               WHERE table_name = 'user'
+                 AND column_name = 'password_change_required_until') THEN
+        UPDATE "user"
+        SET password_change_required = TRUE
+        WHERE password_change_required_until IS NOT NULL;
+    END IF;
+END $$;
