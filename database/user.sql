ad072583c5534b3471bafe074aa2fcd58d4aec19
LeeSeokmin <dltjrals13@naver.com>
2026-09-17 10:15:04 +0900

    chore: 비밀번호 변경 필요 상태 승계 마이그레이션 쿼리 추가

--- database/user.sql
+++ database/user.sql
@@ -4,3 +4,9 @@ ALTER TABLE "user"
 
 COMMENT ON COLUMN "user".password_change_required
     IS '관리자 비밀번호 초기화 후 비밀번호 변경 필요 여부';
+
+-- 기존 password_change_required_until 기반 대상자의 상태를 새 컬럼으로 승계한다.
+-- password_change_required_until 컬럼이 없는 DB(신규 환경)에서는 이 UPDATE만 실패하며, 승계할 대상도 없으므로 무시해도 된다.
+UPDATE "user"
+SET password_change_required = TRUE
+WHERE password_change_required_until IS NOT NULL;
